da0b75cc7eecd84b51f39ffc8bdfa4267243d940
Alvin <alvinallen333@gmail.com>
2023-02-05 22:57:52 +0530

    Finished routes program

--- seperateRoutes/index.js
+++ seperateRoutes/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { router as r1} from "./routes/hello.js";
+import { router as r2} from "./routes/hi.js"
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(r1);
+app.use(r2);
 
 app.get("/", async function (req, res) {
     res.send({ "message": "Hello World" });
