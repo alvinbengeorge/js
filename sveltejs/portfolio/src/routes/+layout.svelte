e560f5494891e5e684ca697eb3419aed7fd89aee
Alvin <alvinallen333@gmail.com>
2023-04-11 08:24:05 +0530

    Renamed folder

---
+++ sveltejs/portfolio/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css";
+</script>
+  
+<div class="bg-gray-800 ">
+    <slot />
+</div>
+
---
+++ sveltejs/portfolio/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+
+    const title = "Chat";
+    const age = 19;
+
+    let messages = [
+        { text: "Hello", bot: "start" },
+        { text: "Hello", bot: "end" },
+    ];
+
+
+</script>
+
+<main>
+    <div class="padding">
+        <h1 class="text-6xl underline content-center">Hello {title}!</h1>
+    </div>
+    <div class="">
+        {#each messages as message}
+            <div class="chat chat-{message.bot}">
+                <p class="chat-bubble">{message.text}</p>
+            </div>
+        {/each}
+    </div>
+</main>
+
+<style global lang="postcss">
+    :global(html) {
+        background-color: theme("colors.gray.100");
+    }
+
+    .padding {
+        padding: 1rem 0rem;
+    }
+</style>
