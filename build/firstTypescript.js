b3aa89ea02f38173d393f93e3aa5eae6b41a54ed
alvinbengeorge <alvinallen333@gmail.com>
2023-04-14 18:17:18 +0530

    Making arrays

--- build/firstTypescript.js
+++ build/firstTypescript.js
@@ -1,2 +1,4 @@
 var a = "Hello world";
 console.log(a);
+var b = [1, 2, 3];
+console.log(b);
--- firstTypescript.ts
+++ firstTypescript.ts
@@ -1,2 +1,4 @@
 let a: string = "Hello world";
 console.log(a);
+let b: Array<number> = [1, 2, 3];
+console.log(b);
